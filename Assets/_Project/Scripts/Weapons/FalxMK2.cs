01a99f4a79b717a68be3aa330773955227e7f896
Alex <citron440@gmail.com>
2026-07-23 11:41:34 +0300

    cleaned up code
    merged the two sica projectile scripts since sica mk1 is basically just sica mk2 without bounces
    added CreatephereAttack in WeaponBase to remove boilerplate and updated weapons to use it

--- Assets/_Project/Scripts/Weapons/FalxMK2.cs
+++ Assets/_Project/Scripts/Weapons/FalxMK2.cs
@@ -19,75 +19,40 @@ namespace Weapons
         protected override void Fire()
         {
             animancer.Play(clip).Time = 0;
-            Collider[] colliders = new Collider[10];
-            int nrOfHits = Physics.OverlapSphereNonAlloc(transform.position + dist * transform.forward, radius, colliders, GlobalSettings.TargetMasks[gameObject.layer]);
-            HashSet<Transform> hits = new();
-            for (int i = 0; i < nrOfHits; i++)
-            {
-                if (!hits.Contains(colliders[i].transform.root))
-                {
-                    hits.Add(colliders[i].transform.root);
-                    EventBus<TakeDamage>.Raise(colliders[i].transform.root.GetInstanceID(), new TakeDamage(1, transform.root, colliders[i]));
-                }
-            }
+            CreateSphereAttack(radius, dist, 1);
         }
         private float boostForce = 1500;
         private float lungeDuration = 1f;
         private float hitCheckInterval = 0.05f;
         protected override void AltFire()
         {
-            StartCoroutine(repeatedAltFireAction());
+            StartCoroutine(RepeatedAltFireAction());
         }
 
         private void InitialSpinAttack()
         {
             animancer.Play(clip).Time = 0;
-            Collider[] colliders = new Collider[10];
-            int nrOfHits = Physics.OverlapSphereNonAlloc(transform.position, 
-                3*radius, 
-                colliders, 
-                GlobalSettings.TargetMasks[gameObject.layer]);
-            HashSet<Transform> spinAttackHits = new();
-            for (int i = 0; i < nrOfHits; i++)
-            {
-                if (!spinAttackHits.Contains(colliders[i].transform.root))
-                {
-                    spinAttackHits.Add(colliders[i].transform.root);
-                    EventBus<TakeDamage>.Raise(colliders[i].transform.root.GetInstanceID(), new TakeDamage(1, transform.root, colliders[i]));
-                }
-            }
+            CreateSphereAttack(3 * radius, 0f, 1);
         }
 
-        private IEnumerator repeatedAltFireAction()
+        private IEnumerator RepeatedAltFireAction()
         {
             InitialSpinAttack();
+
             yield return new WaitForSeconds(0.15f);
+
             BoostPlayer.Invoke(boostForce);
             animancer.Play(clip).Time = 0;
 
             HashSet<Transform> hitsThisLunge = new();
-            float elapsed = 0f;
 
+            float elapsed = 0f;
             while (elapsed < lungeDuration)
             {
-                altFireAction(hitsThisLunge);
+                CreateSphereAttack(radius, dist, 1, hitsThisLunge);
+
                 yield return new WaitForSeconds(hitCheckInterval);
                 elapsed += hitCheckInterval;
-            }
-        }
-
-        private void altFireAction(HashSet<Transform> hits)
-        {
-            Collider[] colliders = new Collider[10];
-            int nrOfHits = Physics.OverlapSphereNonAlloc(transform.position + dist * transform.forward, radius, colliders, GlobalSettings.TargetMasks[gameObject.layer]);
-
-            for (int i = 0; i < nrOfHits; i++)
-            {
-                Transform root = colliders[i].transform.root;
-                if (hits.Add(root))
-                {
-                    EventBus<TakeDamage>.Raise(root.GetInstanceID(), new TakeDamage(1, transform.root, colliders[i]));
-                }
             }
         }
         private void OnDrawGizmosSelected()
